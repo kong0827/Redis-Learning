7af6f42292820c09544c2658eccd63720e078928
kongxiangjin <1351882069@qq.com>
2021-12-14 01:20:20 +0800

    redis基本数据类型

---
+++ Basic-Data-Type/src/main/java/com/BasicDataTypeApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kxj
+ * @date 2021/12/14 00:51
+ * @desc redis基本数据类型的使用，
+ */
+@SpringBootApplication
+public class BasicDataTypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicDataTypeApplication.class, args);
+    }
+}
---
+++ Basic-Data-Type/src/main/java/com/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.config;
+
+/**
+ * @author kxj
+ * @date 2021/12/14 00:58
+ * @desc
+ */
+public class RedisConfig {
+}
